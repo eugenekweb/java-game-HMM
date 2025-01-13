b8039dc6bfa6607868fa6689fbada3df0368822b
Kovalev Evgeny <mr.ekovalev@yandex.com>
2025-01-13 23:26:34 +0300

    GeneratePresetImpl. Add init method.

--- src/programs/GeneratePresetImpl.java
+++ src/programs/GeneratePresetImpl.java
@@ -7,28 +7,38 @@ import com.battle.heroes.army.programs.GeneratePreset;
 import java.util.*;
 
 public class GeneratePresetImpl implements GeneratePreset {
-
+    // коэффициенты для расчета взвешенной эффективности юнита
     private static final double ATTACK_WEIGHT = 0.6;
     private static final double HEALTH_WEIGHT = 0.4;
     private static final double RANGED_ATTACK_MOD = 1.2; // Модификатор для дистанционной атаки
+
+    // поле боя и игровые константы
     private static final int FIELD_WIDTH = 27;
     private static final int FIELD_HEIGHT = 21;
     private static final int FIELD_MAX_LAYERS = 3;
     private static final int MAX_UNITS_PER_TYPE = 11;
 
+    // сквозной счетчик юнитов
     private int unitsCounter = 0;
+    // список уникальных рандомных координат в трех слоях противника
     private List<int[]> randomCoordinates = new ArrayList<>();
 
-    public GeneratePresetImpl() {
+    // инициализируем глобальные переменные
+    private void init() {
+        unitsCounter = 0;
+        randomCoordinates.clear();
         generateCoordinates();
     }
 
+    // метод для генерации рандомных координат
+    // Сложность: O(FIELD_MAX_LAYERS * FIELD_HEIGHT) - это константы, поэтому О(3*21) = О(1)
     private void generateCoordinates() {
         for (int i = 0; i < FIELD_MAX_LAYERS; i++) {
             for (int j = 0; j < FIELD_HEIGHT; j++) {
                 randomCoordinates.add(new int[]{i, j});
             }
         }
+        // здесь встроенный метод со сложностью O(n), где n=63 => О(63) = О(1)
         Collections.shuffle(randomCoordinates, new Random());
     }
 
@@ -44,6 +54,7 @@ public class GeneratePresetImpl implements GeneratePreset {
         return (unit.getBaseAttack() * attackRatio + unit.getHealth() * healthRatio) / unit.getCost();
     }
 
+    // метод для создания юнита на основании прототипа
     private Unit createUnitOfType(Unit origUnit) {
         return new Unit(
                 origUnit.getName() + " " + unitsCounter++, // даем уникальное имя
@@ -61,15 +72,21 @@ public class GeneratePresetImpl implements GeneratePreset {
 
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
-        // Сортируем юнитов по убыванию их взвешенной эффективности
+        init(); // инициализация, на случай повторной генерации
+
+        // сортируем юнитов по убыванию их взвешенной эффективности
+        // Сложность: O(n*log(n)) - стандартная сортировка (на основе TimSort)
         unitList.sort(Comparator.comparingDouble(this::getWeightedPower).reversed());
 
         List<Unit> generatedArmy = new ArrayList<>();
         int currentPoints = 0;
 
+        // добавляем в армию врага юнитов по мере убывания эффективности
+        // Сложность: Цикл по n-типам юнитов + n-циклов с максимальным количеством юнитов - 11
+        // => O(n*11) = O(n)
         for (Unit prototype : unitList) {
+            // максимум юнитов текущего типа мы можем добавить
             int numberOfUnitsOfType = Math.min(MAX_UNITS_PER_TYPE, (maxPoints - currentPoints) / prototype.getCost());
-            System.out.println(numberOfUnitsOfType);
 
             for (int i = 0; i < numberOfUnitsOfType; i++) {
                 generatedArmy.add(createUnitOfType(prototype));
@@ -77,10 +94,14 @@ public class GeneratePresetImpl implements GeneratePreset {
             }
         }
 
-        // Создаем и возвращаем объект армии компьютера
+
+        // создаем и передаем далее объект армии компьютера
         Army enemyArmy = new Army();
         enemyArmy.setUnits(generatedArmy);
         enemyArmy.setPoints(currentPoints);
         return enemyArmy;
     }
 }
+
+// Таким образом, общая суммарная сложность алгоритмов в имплементации:
+// = O( n + n*log(n)) => O(n*log(n))
